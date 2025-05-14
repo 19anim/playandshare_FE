6eba201c9a4efa445004424a32c1d49f7498f3c8
19.anim <nptatdt@gmail.com>
2025-05-15 02:05:30 +0700

    change div background image to img tag

--- src/components/Post/image.component.jsx
+++ src/components/Post/image.component.jsx
@@ -10,10 +10,11 @@ const Image = ({ image, isOverLayout, overAmount, onclickHandler }) => {
     <>
       {isOverLayout ? (
         <div className="relative w-full">
-          <div
-            className="bg-cover w-full h-full bg-center brightness-50"
-            style={{ backgroundImage: `url("${image}")` }}
-          ></div>
+          <img
+            className="w-full h-full object-cover brightness-50"
+            src={image}
+            alt="image_preview"
+          />
           <span
             onClick={onclickHandler}
             className="absolute inset-0 flex justify-center items-center text-white text-3xl cursor-pointer"
@@ -22,28 +23,13 @@ const Image = ({ image, isOverLayout, overAmount, onclickHandler }) => {
           </span>
         </div>
       ) : (
-        <div
+        <img
+          className="w-full h-full object-cover"
           onClick={onclickHandler}
-          className="bg-cover w-full bg-center cursor-pointer"
-          style={{ backgroundImage: `url("${image}")` }}
-        ></div>
+          src={image}
+          alt="image_preview"
+        />
       )}
-      {/* <dialog ref={modalRef} className="modal text-black">
-        <div className="modal-box max-w-screen w-[95%] h-[95%] p-0">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-          </form>
-          <div className="grid lg:grid-cols-3 lg:grid-rows-1 md:grid-rows-3 grid-rows-2 h-full">
-            <div className="col-span-2 lg:row-span-1 md:row-span-2 bg-black flex justify-center items-center">
-              <div
-                style={{ backgroundImage: `url("${image}")` }}
-                className="bg-white w-[70%] h-full bg-cover bg-center"
-              ></div>
-            </div>
-            <div className="px-3 py-10 overflow-scroll">Post detail</div>
-          </div>
-        </div>
-      </dialog> */}
     </>
   );
 };
